abbfd04dc90390d93a11be60b0b84f131ea129d8
baraaj <jeridibaraa@gmail.com>
2022-08-06 00:25:26 +0200

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './navbar.css';
 import {BsSearch} from 'react-icons/bs';
 import {IoMdNotificationsOutline} from 'react-icons/io';
+import img1 from '../images/user.png';
+import {IoIosArrowDown} from 'react-icons/io';
+ 
 const Navbar = () => {
     return (
         <div className="navbar">
               <div className="container">
                    <div className="left">
                    
-            <span className='logo'> TuniFlex </span>  
+                   <span className='logo'> TuniFlix </span>  
                   
                    <span >Homepage  </span>
                    <span >Series  </span>
@@ -17,9 +20,18 @@ const Navbar = () => {
                    <span >My List  </span>
                    </div>
                    <div className='right'>
-                    <BsSearch />
-                      <span>KID</span>
-                      <IoMdNotificationsOutline />
+                    <BsSearch className='icon'/>
+                      <span className='icon'>KID</span>
+                      <IoMdNotificationsOutline className='icon' />
+                      <img src={img1} alt='User image' /> 
+                      <div className=' profile'>  
+
+                      <IoIosArrowDown className='icon' />
+                      <div className='options'>
+                      <span>Settings</span>
+                      <span>Logout</span>
+                      </div>
+                       </div>
                    </div>
              </div>
         </div>
